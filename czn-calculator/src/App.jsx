64bad61ebfd2ed410e1f8aef60e20acfdca69fd1
Ray <rayern@gmail.com>
2025-11-19 22:24:07 +0800

    CharPanel Components

--- czn-calculator/src/App.jsx
+++ czn-calculator/src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import CharPanel from "./assets/components/CharPanel"; 
 
-function App() {
-  const [count, setCount] = useState(0)
-  
-  /*
+/*
     CZN Calculator by Ray Ern
     Neutral cards = 20
     Neutral + Epiphany = 30
@@ -16,13 +12,82 @@ function App() {
     Remove = 0/10/30/50/70 + 20 (base 8)
     Divine = 20
     Convert = 10 
-  */
+*/
 
+function App() {
+  const [char1_values, setChar1Values] = useState(Array(11).fill(0));
+  const [char2_values, setChar2Values] = useState(Array(11).fill(0));
+  const [char3_values, setChar3Values] = useState(Array(11).fill(0));
+
+  const P1 = 1;
+  const P2 = 2;
+  const P3 = 3;
+
+  function increment(character, datarow){
+    var new_state
+    if (character == 1){
+      new_state = char1_values.slice();
+      new_state[datarow]++;
+      setChar1Values(new_state);
+    }
+    else if (character == 2){
+      new_state = char2_values.slice();
+      new_state[datarow]++;
+      setChar2Values(new_state);
+    }
+    else {
+      new_state = char3_values.slice();
+      new_state[datarow]++;
+      setChar3Values(new_state);
+    }
+  };
+
+  function decrement(character, datarow){
+    var new_state
+    if (character == 1){
+      if (char1_values[datarow] == 0){
+        return
+      }
+      new_state = char1_values.slice();
+      new_state[datarow]--;
+      setChar1Values(new_state);
+    }
+    else if (character == 2){
+      if (char2_values[datarow] == 0){
+        return
+      }
+      new_state = char2_values.slice();
+      new_state[datarow]--;
+      setChar2Values(new_state);
+    }
+    else {
+      if (char3_values[datarow] == 0){
+        return
+      }
+      new_state = char3_values.slice();
+      new_state[datarow]--;
+      setChar3Values(new_state);
+    }
+  };
   
-
   return (
     <body>
       <h1>CZN Save Data Calculator</h1>
+      <CharPanel
+        values={char1_values}
+        onIncrement={(row) => increment(P1, row)}
+        onDecrement={(row) => decrement(P1, row)}
+      />
+      <CharPanel
+        values={char2_values}
+        onIncrement={(row) => increment(P2, row)}
+        onDecrement={(row) => decrement(P2, row)}
+      />
+      <CharPanel
+        values={char3_values}
+        onIncrement={(row) => increment(P3, row)}
+        onDecrement={(row) => decrement(P3, row)}
+      />
     </body>
   )
 }
---
+++ czn-calculator/src/assets/components/CharPanel.jsx
@@ -0,0 +1,166 @@
+export default function CharPanel({values, onIncrement, onDecrement}) {
+  console.log(values)
+  const COST = 10
+  const COPY = 0
+  const REMOVE = 1
+  const REMBASIC = 2
+  const CONVERT = 3
+  const NCARDS = 4
+  const NEPI = 5
+  const NDIV = 6
+  const SCARDS = 7
+  const MCARDS = 8
+  const MDIV = 9
+  
+  var tier = 1
+  var cost_limit = limitCalc()
+
+  function limitCalc(){
+    return tier * 10 + 20
+  }
+
+  function copyCalc(num) {
+    
+    if (num >= 2) {
+      return (num - 2) * 20 + 10;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  function remBasic(num){
+    if (num == 0){
+      return 0;
+    }
+    
+    return copyCalc(num) + 20;
+  }
+  
+  return (
+    <div class="box"> 
+      <div class="innerbox">
+        <h3>
+          Deck Modifying
+        </h3>
+        <div class="row">
+          <div>{values[COPY]} Copy</div>
+          <div class="cost">{copyCalc(values[COPY])}</div>
+          <button class="plus" onClick={() => {
+            onIncrement(COPY);
+          }}>+</button>
+          <button class="minus" onClick={() => {
+            onDecrement(COPY);
+          }}>-</button>
+
+        </div>
+        <div class="row">
+          <div>{values[REMOVE]} Remove</div>
+          <div class="cost">{copyCalc(values[REMOVE])}</div>
+          <button class="plus" onClick={() => {
+            onIncrement(REMOVE);
+          }}>+</button>
+          <button class="minus" onClick={() => {
+            onDecrement(REMOVE);
+          }}>-</button>
+        </div>
+        <div class="row">
+          <div>{values[REMBASIC]} Remove Basic</div>
+          <div class="cost">{remBasic(values[REMBASIC])}</div>
+          <button class="plus" onClick={() => {
+            onIncrement(REMBASIC);
+          }}>+</button>
+          <button class="minus" onClick={() => {
+            onDecrement(REMBASIC);
+          }}>-</button>
+        </div>
+        <div class="row">
+          <div>{values[CONVERT]} Convert</div>
+          <div class="cost">{values[CONVERT] * 10}</div>
+          <button class="plus" onClick={() => {
+            onIncrement(CONVERT);
+          }}>+</button>
+          <button class="minus" onClick={() => {
+            onDecrement(CONVERT);
+          }}>-</button>
+        </div>
+      </div>
+
+      <div class="innerbox">
+        <h3>
+            Neutral Cards
+        </h3>
+        <div class="row">
+          <div>{values[NCARDS]} Cards</div>
+          <div class="cost">{values[NCARDS] * 20}</div>
+          <button class="plus" onClick={() => {
+            onIncrement(NCARDS);
+          }}>+</button>
+          <button class="minus" onClick={() => {
+            onDecrement(NCARDS);
+          }}>-</button>
+        </div>
+        <div class="row">
+          <div>{values[NEPI]} Epiphany</div>
+          <div class="cost">{values[NEPI] * 10}</div>
+          <button class="plus" onClick={() => {
+            onIncrement(NEPI);
+          }}>+</button>
+          <button class="minus" onClick={() => {
+            onDecrement(NEPI);
+          }}>-</button>
+        </div>
+        <div class="row">
+          <div>{values[NDIV]} Divine Epiphany</div>
+          <div class="cost">{values[NDIV] * 30}</div>
+          <button class="plus" onClick={() => {
+            onIncrement(NDIV);
+          }}>+</button>
+          <button class="minus" onClick={() => {
+            onDecrement(NDIV);
+          }}>-</button>
+        </div>
+      </div>
+      <div class="innerbox">
+        <h3>
+            Seasonal Cards
+        </h3>
+        <div class="row">
+          <div>{values[SCARDS]} Forbidden Cards</div>
+          <div class="cost">{values[SCARDS] * 20}</div>
+          <button class="plus" onClick={() => {
+            onIncrement(SCARDS);
+          }}>+</button>
+          <button class="minus" onClick={() => {
+            onDecrement(SCARDS);
+          }}>-</button>
+        </div>
+      </div>
+      <div class="innerbox">  
+        <h3>
+            Monster Cards
+        </h3>
+        <div class="row">
+          <div>{values[MCARDS]} Cards</div>
+          <div class="cost">{values[MCARDS] * 80}</div>
+          <button class="plus" onClick={() => {
+            onIncrement(MCARDS);
+          }}>+</button>
+          <button class="minus" onClick={() => {
+            onDecrement(MCARDS);
+          }}>-</button>
+        </div>
+        <div class="row">
+          <div>{values[MDIV]} Divine Epiphany</div>
+          <div class="cost">{values[MDIV] * 20}</div>
+          <button class="plus" onClick={() => {
+            onIncrement(MDIV);
+          }}>+</button>
+          <button class="minus" onClick={() => {
+            onDecrement(MDIV);
+          }}>-</button>
+        </div>
+      </div>
+    </div>
+  );
+}
