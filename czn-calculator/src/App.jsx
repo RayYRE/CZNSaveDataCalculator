d78d1ff13df043d450212f794f5a802d718ffede
Ray <rayern@gmail.com>
2025-11-19 16:38:22 +0800

    React setup

---
+++ czn-calculator/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  /*
+    CZN Calculator by Ray Ern
+    Neutral cards = 20
+    Neutral + Epiphany = 30
+    Forbidden (Season) = 20 
+    Monster = 80 (no neutral tax)
+    Copy = 0/10/30/50/70
+    Remove = 0/10/30/50/70 + 20 (base 8)
+    Divine = 20
+    Convert = 10 
+  */
+
+  
+
+  return (
+    <body>
+      <h1>CZN Save Data Calculator</h1>
+    </body>
+  )
+}
+
+export default App
